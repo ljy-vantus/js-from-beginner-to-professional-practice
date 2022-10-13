58591a8658c0005bb711d61f145d7694ef13de5a
liangjinyong123 <liangjy881@naver.com>
2022-10-13 13:11:39 +0900

    First commit

---
+++ ch03_javascript_multiple_values/01_array.js
@@ -0,0 +1,15 @@
+arr1 = new Array("purple", "green", "yellow");
+arr2 = ["black", "orange", "pink"]
+console.log(arr1);
+console.log(arr2);
+arr3 = new Array(10);
+arr4 = [10];
+console.log(arr3);
+console.log(arr4);
+
+let arr = ["hi there", 5, true];
+console.log(typeof arr[0]);
+console.log(typeof arr[1]);
+console.log(typeof arr[2]);
+
+
